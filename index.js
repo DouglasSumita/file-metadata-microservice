8fc06e1c17d3f67df66ef15090a64166c6569d1c
Douglas Sumita <douglas.sumita@gmail.com>
2024-03-05 16:50:07 +0000

    Adicionado pacote http-status-codes para lidar com codigo de status de requisições. Adicionado pacote multer para lidar com upload de arquivos, por padrão está sendo gravado na memória. Adicionado rota na api para receber a requisição POST no endpoint /api/fileanalyse utilizando o fieldname 'upfile' e retornado um objeto com nome, tipo e tamanho do arquivo.

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
+const { StatusCodes } = require('http-status-codes');
+const multer = require('multer')
+const upload = multer({ dest: 'uploads/' })
 
 var app = express();
 
@@ -11,7 +14,21 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res, next) => {
+  if (!req.file.originalname) {
+    res.status(StatusCodes.BAD_REQUEST).send({
+      error: 'Invalid file'
+    });
+    return;
+  }
+  next();
+}, (req, res) => {
+  res.status(StatusCodes.CREATED).send({
+    name: req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size
+  });
+})
 
 
 const port = process.env.PORT || 3000;
